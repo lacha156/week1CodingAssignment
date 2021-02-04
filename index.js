dedb4584c4d98fc83354a4deead2e930a3454fa8
Luis Chavez <loschavez@Luiss-Air.domain>
2021-02-03 18:32:18 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var itemPrice = 100.00;
+var amountOfMoneyInWallet = 300.00;
+
+var numberOfFriends = 5;
+var ageInYears = 35
+
+var firstName = "Luis";
+var middleInitial = "A";
+var lastName = "Chavez";
+
+var newAmountOfMoneyInWallet = amountOfMoneyInWallet - itemPrice;
+var friendsPerYear = numberOfFriends / ageInYears;
+var fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+
+console.log('My full name is:' +fullName);
+console.log('Bought a basketball for:' +itemPrice);
+console.log('I have this much money in my wallet: ' + amountOfMoneyInWallet);
+console.log('I have this many friends:' +numberOfFriends);
+console.log('I am currently:'+ageInYears);
+console.log('After buying a basketball I have this much in my wallet: ' + newAmountOfMoneyInWallet);
+console.log('I make this many new friends each year:' + friendsPerYear);
+console.log('My name is: ' +firstName);
+console.log('My last name is: ' +lastName);
+
+
+
+
+
+
+
